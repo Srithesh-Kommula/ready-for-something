4e93b4a5a276abf0375adbe072f10853616556c3
Srithesh Kommula <143762447+Srithesh-Kommula@users.noreply.github.com>
2026-02-07 10:54:51 -0800

    Update config.js
    Hamsaro

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/detzbf7fy/video/upload/v1770488682/Buttabomma_-_SenSongsMp3.Co_fqsbsm.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/detzbf7fy/video/upload/1-Hamsaro-SenSongsMp3.Co_elinj8.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
